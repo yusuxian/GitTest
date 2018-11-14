eaf58f45ba9abf1c3df4005340e283b8e9379792
tom <goodmorning@qst.com>
2018-11-14 08:18:59 +0800

    this  is  merge  hot_fix

--- src/main/java/com/qst/Person.java
+++ src/main/java/com/qst/Person.java
@@ -1,15 +1,10 @@
 package com.qst;
 
 public class Person {
-<<<<<<< HEAD
-  public static void main(String[] args) {
-	System.out.println("我是test1");
-	System.out.println("我是test");
-}
-=======
+
     public static void main(String[] args) {
 		System.out.println("我是test");
 		System.out.println("hot  fix...");
 	}
->>>>>>> refs/heads/hot_fix
+
 }
